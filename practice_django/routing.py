f1e74e3971d5011b324bd81753949a833b6ac22d
narekas <apresyan026@gmail.com>
2023-03-16 14:42:24 +0400

    three

---
+++ practice_django/routing.py
@@ -0,0 +1,12 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+
+application = ProtocolTypeRouter(
+    {
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                # chat.routing.websocket_urlpatterns)
+        )
+    ),
+})
--- practice_django/settings.py
+++ practice_django/settings.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+
+# django-debug-toolbar
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 """
 Django settings for practice_django project.
 
@@ -57,6 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # https://docs.djangoproject.com/en/4.1/ref/contrib/humanize/
     'django.contrib.humanize',
+    'django_extensions',
 
     # install pasc
     'allauth',
@@ -65,7 +75,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
 
-
+    'debug_toolbar',
+    # 'channels',
     'crispy_forms',
     'ckeditor',
     # Мои приложения
@@ -85,6 +96,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware", # django-debug-toolbar
 ]
 
 ROOT_URLCONF = 'practice_django.urls'
@@ -238,3 +250,39 @@ CKEDITOR_CONFIGS = {
 }
 
 # End django-ckeditor
+
+
+# django-channels
+
+# ASGI_APPLICATION = "practice_django.routing.application"
+
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels.layers.InMemoryChannelLayer'
+#     },
+# }
+
+
+# End django-channels
+
+
+# email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+
+# End email
+
+GOOGLE_RECAPTCHA_SECRET_KEY = os.getenv('GOOGLE_RECAPTCHA_SECRET_KEY')
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
